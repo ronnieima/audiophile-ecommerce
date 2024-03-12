6e9a3a27919f91c4e15d67266e6c0b7b2cbac20e
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-12 19:05:25 +0100

    install shadcn

--- src/app/components/Hero.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Hero() {
-  return <section className="bg-black text-white">Hero</section>;
-}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./ui/button";
+
+export default function Hero() {
+  return (
+    <section className="relative h-full bg-[url('/home/mobile/image-header.jpg')] bg-cover bg-center bg-no-repeat text-white">
+      <header className="flex h-full flex-col items-center justify-center gap-4 px-4 py-32 text-center">
+        <span className="text-overline">NEW PRODUCT</span>
+        <h3>XX99 MARK II HEADPHONES</h3>
+        <p className="text-lightgray">
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <Button className="text-xs uppercase" variant="default">
+          See product
+        </Button>
+      </header>
+    </section>
+  );
+}
