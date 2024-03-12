c2fc9a5ac2a15f018eb4f652869a448219a2aa76
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-12 18:13:29 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#D87D4A",
+      secondary: "#101010",
+      gray: "#F1F1F1",
+      lightgray: "#FAFAFA",
+      accent: "#FBAF85",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
