0a957295aa3b260ff9cef13da7bd67c46f016803
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-13 21:14:05 +0100

    start on desktop layout

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,36 +1,55 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
 export default function CallToAction() {
   return (
-    <section className="flex flex-col items-center justify-center gap-16 px-4 pb-32 text-center">
-      <Image
-        src={"/assets/shared/mobile/image-best-gear.jpg"}
-        height={0}
-        width={0}
-        sizes="100vw"
-        className="h-auto w-full rounded-lg sm:hidden"
-        alt="a black-and-white image of a man wearing headphones posing"
-      />
-      <Image
-        src={"/assets/shared/tablet/image-best-gear.jpg"}
-        height={0}
-        width={0}
-        sizes="100vw"
-        className="hidden h-auto w-full rounded-lg sm:block"
-        alt="a black-and-white image of a man wearing headphones posing"
-      />
-      <h2 className="sm:max-w-xl">
-        Bringing you the <span className="text-primary">best</span> audio gear
-      </h2>
-      <p className="sm:max-w-lg">
-        Located at the heart of New York City, Audiophile is the premier store
-        for high end headphones, earphones, speakers, and audio accessories. We
-        have a large showroom and luxury demonstration rooms available for you
-        to browse and experience a wide range of our products. Stop by our store
-        to meet some of the fantastic people who make Audiophile the best place
-        to buy your portable audio equipment.
-      </p>
+    <section className="px-4 pb-32 text-center">
+      <div
+        className={cn(
+          "flex flex-col items-center justify-center gap-16 ",
+          "lg:mx-auto lg:max-w-7xl lg:flex-row-reverse",
+        )}
+      >
+        <Image
+          src={"/assets/shared/mobile/image-best-gear.jpg"}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full rounded-lg sm:hidden"
+          alt="a black-and-white image of a man wearing headphones posing"
+        />
+        <Image
+          src={"/assets/shared/tablet/image-best-gear.jpg"}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="hidden h-auto w-full rounded-lg sm:block lg:hidden"
+          alt="a black-and-white image of a man wearing headphones posing"
+        />
+        <Image
+          src={"/assets/shared/desktop/image-best-gear.jpg"}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className=" hidden h-auto w-2/4 rounded-lg lg:block"
+          alt="a black-and-white image of a man wearing headphones posing"
+        />
+        <div className="flex flex-col items-center justify-center gap-8 lg:items-start lg:text-left">
+          <h2 className="sm:max-w-xl">
+            Bringing you the <span className="text-primary">best</span> audio
+            gear
+          </h2>
+          <p className="sm:max-w-lg">
+            Located at the heart of New York City, Audiophile is the premier
+            store for high end headphones, earphones, speakers, and audio
+            accessories. We have a large showroom and luxury demonstration rooms
+            available for you to browse and experience a wide range of our
+            products. Stop by our store to meet some of the fantastic people who
+            make Audiophile the best place to buy your portable audio equipment.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,55 +5,63 @@ import { cn } from "@/lib/utils";
 
 export default function Products() {
   return (
-    <section className={cn("flex flex-col gap-8  px-4 text-center", "sm:px-8")}>
+    <section>
       <div
         className={cn(
-          "relative flex flex-col items-center justify-center gap-8 rounded-lg bg-primary bg-patternCircles bg-cover bg-[center_-7rem] bg-no-repeat p-8 py-32  text-white",
+          "flex flex-col gap-8  px-4 text-center",
+          "sm:px-8",
+          "lg:mx-auto lg:max-w-7xl",
         )}
       >
-        <div className="w-1/2 sm:w-1/4">
-          <Image
-            src={"/assets/home/mobile/image-speaker-zx9.png"}
-            className=" h-auto w-full"
-            sizes="100vw"
-            width={0}
-            height={0}
-            alt="ZX9 Speaker"
-          />
-        </div>
-        <h2 className="max-w-[16rem]">ZX9 Speaker</h2>
-        <p className="max-w-xs">
-          Upgrade to premium speakers that are phenomenally built to deliver
-          truly remarkable sound.
-        </p>
-        <Button
-          className="border-none bg-black text-white hover:bg-white hover:text-black"
-          variant={"secondary"}
-        >
-          See product
-        </Button>
-      </div>
-
-      <div
-        className={cn(
-          "flex h-96 flex-col items-start justify-center gap-8 rounded-lg bg-[url('/assets/home/mobile/image-speaker-zx7.jpg')] bg-cover bg-right p-4 ",
-          "sm:bg-[url('/assets/home/tablet/image-speaker-zx7.jpg')] sm:px-16",
-        )}
-      >
-        <h3>ZX7 Speaker</h3>
-        <Button variant={"secondary"}>See product</Button>
-      </div>
-
-      <div className="flex  flex-col items-center justify-center gap-8 sm:flex-row">
         <div
           className={cn(
-            "h-[16rem] w-full rounded-lg bg-[url('/assets/home/mobile/image-earphones-yx1.jpg')] bg-cover",
-            "sm:bg-[url('/assets/home/tablet/image-earphones-yx1.jpg')]",
+            "relative flex h-full w-full flex-col items-center justify-center gap-8 rounded-lg bg-primary bg-patternCircles bg-cover bg-[center_-7rem] bg-no-repeat p-8 py-32 text-white",
           )}
-        ></div>
-        <div className="flex h-[16rem] w-full flex-col items-start justify-center gap-8 rounded-lg bg-gray p-8 py-12">
-          <h3 className="text-left">YX1 Earphones</h3>
+        >
+          <div className="w-1/2 sm:w-1/4">
+            <Image
+              src={"/assets/home/mobile/image-speaker-zx9.png"}
+              className=" h-auto w-full"
+              sizes="100vw"
+              width={0}
+              height={0}
+              alt="ZX9 Speaker"
+            />
+          </div>
+          <h2 className="max-w-[16rem]">ZX9 Speaker</h2>
+          <p className="max-w-xs">
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <Button
+            className="border-none bg-black text-white hover:bg-white hover:text-black"
+            variant={"secondary"}
+          >
+            See product
+          </Button>
+        </div>
+
+        <div
+          className={cn(
+            "flex h-96 flex-col items-start justify-center gap-8 rounded-lg bg-[url('/assets/home/mobile/image-speaker-zx7.jpg')] bg-cover bg-right p-4 ",
+            "sm:bg-[url('/assets/home/tablet/image-speaker-zx7.jpg')] sm:px-16",
+          )}
+        >
+          <h3>ZX7 Speaker</h3>
           <Button variant={"secondary"}>See product</Button>
+        </div>
+
+        <div className="flex  flex-col items-center justify-center gap-8 sm:flex-row">
+          <div
+            className={cn(
+              "h-[16rem] w-full rounded-lg bg-[url('/assets/home/mobile/image-earphones-yx1.jpg')] bg-cover",
+              "sm:bg-[url('/assets/home/tablet/image-earphones-yx1.jpg')]",
+            )}
+          ></div>
+          <div className="flex h-[16rem] w-full flex-col items-start justify-center gap-8 rounded-lg bg-gray p-8 py-12">
+            <h3 className="text-left">YX1 Earphones</h3>
+            <Button variant={"secondary"}>See product</Button>
+          </div>
         </div>
       </div>
     </section>
