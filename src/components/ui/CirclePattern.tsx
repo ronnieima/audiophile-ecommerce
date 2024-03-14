5c75b5b0187a0ccfe3660257e84af6603680d708
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-14 07:27:38 +0100

    fix prop

--- src/components/ui/CirclePattern.tsx
+++ src/components/ui/CirclePattern.tsx
@@ -4,7 +4,7 @@ type CirclePatternProps = { size?: number };
 export default function CirclePattern({ size = 944 }: CirclePatternProps) {
   return (
     <svg width={size} height={size} xmlns="http://www.w3.org/2000/svg">
-      <g stroke="#FFF" fill="none" fill-rule="evenodd" opacity=".202">
+      <g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202">
         <circle cx={size / 2} cy={size / 2} r="235.5" />
         <circle cx={size / 2} cy={size / 2} r="270.5" />
         <circle cx={size / 2} cy={size / 2} r="471.5" />
