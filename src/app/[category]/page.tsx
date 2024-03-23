11e5b3421f07d47efa1e2e4b0a5f002897f983c4
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-23 09:37:06 +0100

    fixed UI bugs

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -15,7 +15,7 @@ export default async function CategoryPage({
   const filteredProducts = await getProducts(category);
   if (!filteredProducts) return notFound();
   return (
-    <main>
+    <main className="flex flex-col items-stretch justify-center gap-32 ">
       <header
         className={cn(
           "flex h-32 items-center justify-center bg-black text-center uppercase text-white",
@@ -27,7 +27,7 @@ export default async function CategoryPage({
       <section
         className={cn(
           "space-y-16 px-4 py-16",
-          "sm:space-y-24 sm:px-8 sm:py-24",
+          "sm:space-y-48 sm:px-8 sm:py-24",
         )}
       >
         {filteredProducts.map((product) => (
