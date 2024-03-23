652917c163ceed5d81fd106f2e2c4ef17b563d78
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-23 08:54:03 +0100

    remove  dotenv

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,7 @@
 import { NeonQueryFunction, neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-import { config } from "dotenv";
-config({ path: ".env.local" });
+
 const sql: NeonQueryFunction<boolean, boolean> = neon(
   process.env.DATABASE_URL!,
 );
